0aa0c037c353a40c4c047c77879ddb5eb05f1b04
Usmaelabdureman <uabdureman@gmail.com>
2023-05-23 10:51:01 +0300

    add register and sign in

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,3 +1,5 @@
-export default function SigninPage() {
-    return <div >Sign in </div>
+import AuthForm from "@/components/AuthForm";
+
+export default function Signin() {
+  return <AuthForm mode="signin" />;
 }

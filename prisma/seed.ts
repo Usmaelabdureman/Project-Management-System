e49de4c8850e27ecec3d8e3f06913398ae6b586d
Usmaelabdureman <uabdureman@gmail.com>
2023-06-07 13:52:47 +0300

    update git workflow

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,7 +23,7 @@ async function main() {
       email: "user@email.com",
       firstName: "User",
       lastName: "Person",
-      password: await hashPassword("password"),
+      password: await hashPassword('password'),
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
